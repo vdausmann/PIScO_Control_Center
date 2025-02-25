4bea98cbf1f6b691d0ee59b6a069f10ed16fe42a
Tim3720 <timkalsb@gmail.com>
2025-02-25 22:19:34 +0100

    Clean up

--- ControlCenter/Modules/BackgroundCorrection/include/reader.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-void getFiles(std::string path, std::vector<std::string>& files);
--- ControlCenter/Modules/BackgroundCorrection/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "threads.hpp"
-#include <iostream>
-
-int main()
-{
-    ThreadManager tm(1, 1);
-
-    return 0;
-}
